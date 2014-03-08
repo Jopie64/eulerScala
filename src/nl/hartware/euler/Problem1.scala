1e9b94fe6c02509a04b3c21d51cce927268df476
Johan 't Hart <johanthart@gmail.com>
2014-03-08 21:56:15 +0100

    Problem 1. Quick and dirty

--- src/nl/hartware/euler/Problem1.scala
+++ src/nl/hartware/euler/Problem1.scala
@@ -2,6 +2,13 @@ package nl.hartware.euler
 
 class Problem1 extends Problem[Int] {
 	override def answer() : Int = {
-	  1
+	  var sum = 0
+	  var i = 0
+	  for(i <- 1 to 999) {
+	    if(i % 5 == 0 || i % 3 == 0) {
+	      sum += i
+	    }
+	  }
+	  sum
 	}
 }
