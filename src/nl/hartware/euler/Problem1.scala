00992375ea49cda9e90bf319346d8dff45f211b6
Johan 't Hart <johanthart@gmail.com>
2014-03-08 22:23:21 +0100

    Problem 1 using scala power

--- src/nl/hartware/euler/Problem1.scala
+++ src/nl/hartware/euler/Problem1.scala
@@ -2,6 +2,11 @@ package nl.hartware.euler
 
 class Problem1 extends Problem[Int] {
 	override def answer() : Int = {
+	  List.tabulate(999)(n => n+1).filter(n => n % 3 == 0 || n % 5 == 0).fold(0)(_ + _)
+	}
+	
+	//qad = Quick And Dirty
+	def answer_qad() : Int = {
 	  var sum = 0
 	  var i = 0
 	  for(i <- 1 to 999) {
