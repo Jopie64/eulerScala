3b9b8ecfed4dbe4a0018b275bdea3a9b6254edce
Johan 't Hart <johanthart@gmail.com>
2014-03-09 00:06:25 +0100

    Problem 2: Scala Power!

--- src/nl/hartware/euler/Problem2.scala
+++ src/nl/hartware/euler/Problem2.scala
@@ -1,7 +1,15 @@
 package nl.hartware.euler
 
 class Problem2 extends Problem[Int] {
+
 	override def answer() = {
+	    lazy val fibs : Stream[Int] = {
+	      0 #:: 1 #:: fibs.zip(fibs.tail).map(n => n._1 + n._2)
+	    }
+    	fibs.view.takeWhile(_ < 4000000).filter(_ % 2 == 0).sum
+	}
+
+	def answer_qad() = {
 	  var i = 1
 	  var p = 0
 	  var answer = 0
