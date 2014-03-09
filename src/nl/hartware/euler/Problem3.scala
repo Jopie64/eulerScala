4910dba228e23e61982c8434fdddfdabaff3fe1c
Johan 't Hart <johanthart@gmail.com>
2014-03-09 22:07:00 +0100

    Problem 3: Working with example number

--- src/nl/hartware/euler/Problem3.scala
+++ src/nl/hartware/euler/Problem3.scala
@@ -5,9 +5,7 @@ class Problem3 extends Problem[Int] {
 	override def answer() : Int = {
 	  lazy val prime : Stream[Int] = 2 #:: (Stream.from(3).filter(n => {
 	    prime.takeWhile(n2 => n2 * n2 <= n).forall(n2 => n % n2 != 0)}))
-	  
-	  prime take 100 foreach println
-	  intWrapper(1) to 10 sum
-//	  lazy val prime : Stream[Int] = { 1 to 10}
+
+	  prime.takeWhile(n => n*n <= number).filter(n => number % n == 0).last
 	}
 }
