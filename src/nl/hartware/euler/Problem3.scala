3965a9006a33616b50ff55889c3a38fad0335acf
Johan 't Hart <johanthart@gmail.com>
2014-03-09 22:52:25 +0100

    Problem 3: Less powerfull way that works for big number

--- src/nl/hartware/euler/Problem3.scala
+++ src/nl/hartware/euler/Problem3.scala
@@ -1,11 +1,17 @@
 package nl.hartware.euler
 
 class Problem3 extends Problem[Int] {
-	val number = 13195
+//	val number = 13195
+	val number = 600851475143L
 	override def answer() : Int = {
-	  lazy val prime : Stream[Int] = 2 #:: (Stream.from(3).filter(n => {
-	    prime.takeWhile(n2 => n2 * n2 <= n).forall(n2 => n % n2 != 0)}))
+//	  lazy val prime : Stream[Int] = 2 #:: (Stream.from(3).filter(n => {
+//	    prime.takeWhile(n2 => n2 * n2 <= n).forall(n2 => n % n2 != 0)}))
 
-	  prime.takeWhile(n => n*n <= number).filter(n => number % n == 0).last
+//	  prime.takeWhile(n => n*n <= number).filter(n => number % n == 0).last
+	  var rest = number
+	  var ret = 0
+	  (2 to Math.sqrt(rest).toInt).foreach( n => while(rest % n == 0) { ret = n; rest /= n })
+	  
+	  ret
 	}
 }
