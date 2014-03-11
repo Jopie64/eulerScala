5a703ff35d69b2f86c345153c2d6e0e67be34b96
Johan 't Hart <johanthart@gmail.com>
2014-03-11 23:59:17 +0100

    Problem11: Add

--- src/Main.scala
+++ src/Main.scala
@@ -3,13 +3,17 @@ import nl.hartware.euler._
 
 
 object Main extends App {
-  println("problem1: " + (new Problem1()).answer())
-  println("problem2: " + (new Problem2()).answer())
-  println("problem3: " + (new Problem3()).answer())
-  println("problem5: " + (new Problem5()).answer())
-  println("problem6: " + (new Problem6()).answer())
-  println("problem7: " + (new Problem7()).answer())
-  println("problem8: " + (new Problem8()).answer())
-  println("problem9: " + (new Problem9()).answer())
-  println("problem10: "+ (new Problem10()).answer())
+  def oldProblems() = {
+	  println("problem1: " + (new Problem1()).answer())
+	  println("problem2: " + (new Problem2()).answer())
+	  println("problem3: " + (new Problem3()).answer())
+	  println("problem5: " + (new Problem5()).answer())
+	  println("problem6: " + (new Problem6()).answer())
+	  println("problem7: " + (new Problem7()).answer())
+	  println("problem8: " + (new Problem8()).answer())
+	  println("problem9: " + (new Problem9()).answer())
+	  println("problem10: "+ (new Problem10()).answer())
+  }
+
+  println("problem11: "+ (new Problem11()).answer())
 }
