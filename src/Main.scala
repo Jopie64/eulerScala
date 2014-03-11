96ea26c5bbce8f0bffb0917021acaa1491af6ee3
Johan 't Hart <johanthart@gmail.com>
2014-03-11 20:07:29 +0100

    Add: Problem7

--- src/Main.scala
+++ src/Main.scala
@@ -1,10 +1,10 @@
 import nl.hartware.euler._
 
-
 object Main extends App {
   println("problem1: " + (new Problem1()).answer())
   println("problem2: " + (new Problem2()).answer())
   println("problem3: " + (new Problem3()).answer())
   println("problem5: " + (new Problem5()).answer())
   println("problem6: " + (new Problem6()).answer())
+  println("problem7: " + (new Problem7()).answer())
 }
---
+++ src/nl/hartware/euler/Problem7.scala
@@ -0,0 +1,7 @@
+package nl.hartware.euler
+
+class Problem7 extends Problem[Int] {
+	override def answer():Int = {
+	  Utils.primes(10000)
+	}
+}
